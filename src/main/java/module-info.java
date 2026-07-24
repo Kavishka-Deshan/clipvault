c98abd3faba7a42661cacc58ed3dc178b6135e2e
Kumuditha Tharinda <140329762+mr-kumuditha@users.noreply.github.com>
2026-07-24 11:34:09 +0530

    add module-info descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.clipcycle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // Let JavaFX reflectively access these packages (needed for FXML loading)
+    opens com.clipcycle to javafx.fxml;
+
+    exports com.clipcycle;
+}
